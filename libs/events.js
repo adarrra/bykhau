777a66b13614e3e539bc8ed1548de0fa2e0660ef
gdmipro <gdmipro@gmail.com>
2020-05-08 01:07:26 +0200

    08.05

--- libs/events.js
+++ libs/events.js
@@ -1,18 +1,14 @@
-
-
-
-let card2 = [
-    {
+let card2 = [{
         pic: 'page_events/e1.png',
-        title: 'Лекция “Айти в городе”', 
-        descr: 'Прекрасные высокотехнологичные авто благодаря нашему стартапу появились в сентябере 2019 года над мирным небом Быхова',
+        title: '23-24 апреля Стартует 1 набор курса «Гид по IT-профессиям».',
+        descr: 'На курсе Вы узнаете много нового о сфере IT, а также важный секрет - в IT есть не только программисты! Для обучения вам понадобится лишь компьютер с выходом в интернет и регистрация на курс.',
         link: 'link1.html',
         stl: 0
     },
     {
         pic: 'page_events/e2.png',
-        title: 'Семинар “Войти в айти”',
-        descr: 'Прекрасные высокотехнологичные авто благодаря нашему стартапу появились в сентябере 2019 года над мирным небом Быхова',
+        title: '25-26 апреля Стартует 2 набор на курса «Гид по IT-профессиям»',
+        descr: 'Задумываетесь о светлом будущем, о том, как обеспечить себя, семью, детей? Ищете новую работу или хотите заняться чем-то более интересным? Вам к нам! На курсе вы узнаете общую информацию об IT-профессиях и какие из них наиболее перспективные. Детально ознакомитесь с основными профессиями и поймете, чем они отличаются, какая подошла бы именно Вам.',
         link: 'link2.html',
         stl: 0
     },
@@ -25,25 +21,25 @@ let card2 = [
     }
 ];
 
-let sc2='';
-for (c2=0; c2<card2.length; c2++) {
-    if (card2[c2].stl == 0) {  
-    sc2 += ` <div><a href="${card2[c2].link}"><img class="card_img" src="../img/${card2[c2].pic}" alt=""></a>
+let sc2 = '';
+for (c2 = 0; c2 < card2.length; c2++) {
+    if (card2[c2].stl == 0) {
+        sc2 += ` <div><a href="${card2[c2].link}"><img class="card_img" src="../img/${card2[c2].pic}" alt=""></a>
     <h3 class="card_title"><a style="color: #fff" href="${card2[c2].link}">${card2[c2].title}</a></h3>
         <div class="hc1">
         <span><a href="${card2[c2].link}">${card2[c2].descr}</a></span>
         </div>
         <div class="card_arrow"><a href="${card2[c2].link}"><img src="../img/arrow10.svg" alt=""></a></div>    
     </div>  `;
-}  else {
-    sc2 += `  <div onclick="forma()" style="cursor: pointer; box-shadow: none; border: 0"><img class="card_img" src="../img/${card2[c2].pic}" alt="">
+    } else {
+        sc2 += `  <div onclick="forma()" style="cursor: pointer; box-shadow: none; border: 0"><img class="card_img" src="../img/${card2[c2].pic}" alt="">
     <h3 class="card_title" style="color: #3725E4; text-shadow: none; text-align: center">${card2[c2].title}</h3>
         <div class="hc1" style="text-align: center">
         <span>${card2[c2].descr}</span>
         </div>
         <div class="card_arrow"><img src="../img/arrow10.svg" alt=""></div>    
     </div> `;
-}
+    }
 
 }
 
@@ -52,10 +48,9 @@ car2.innerHTML = sc2;
 
 
 
-let card4 = [
-    {
+let card4 = [{
         pic: 'page_events/e3.png',
-        title: 'Пятничное чаепитие для избранных', 
+        title: 'Пятничное чаепитие для избранных',
         descr: 'Прекрасные высокотехнологичные авто благодаря нашему стартапу появились в сентябере 2019 года над мирным небом Быхова',
         link: 'link4.html',
     },
@@ -67,15 +62,15 @@ let card4 = [
     },
     {
         pic: 'page_events/e5.png',
-        title: 'Правила дистанционного обучения. Лекция', 
+        title: 'Правила дистанционного обучения. Лекция',
         descr: 'Прекрасные высокотехнологичные авто благодаря нашему стартапу появились в сентябере 2019 года над мирным небом Быхова',
         link: 'link6.html',
     },
 ];
 
-let sc4='';
-for (c4=0; c4<card4.length; c4++) {
-   
+let sc4 = '';
+for (c4 = 0; c4 < card4.length; c4++) {
+
     sc4 += ` <div><a href="${card4[c4].link}"><img class="card_img" src="../img/${card4[c4].pic}" alt=""></a>
     <h3 class="card_title"><a style="color: #fff" href="${card4[c4].link}">${card4[c4].title}</a></h3>
         <div class="hc1">
@@ -89,5 +84,3 @@ for (c4=0; c4<card4.length; c4++) {
 
 //  console.log(s1);
 car4.innerHTML = sc4;
-
-
